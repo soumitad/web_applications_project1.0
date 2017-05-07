65bcdfc9973433191e151ab6a41d526e045616fe
soumitad <soumita.sv@gmail.com>
2017-05-07 11:41:07 -0400

    Header changes

--- view/styleHeader.php
+++ view/styleHeader.php
@@ -20,17 +20,7 @@
             </div>
         </div>    
     </div>
-        <nav class="navbar navbar-default">
-    <div class="container-fluid">
-    <div class="navbar-header">
-    <!--  <a class="navbar-brand" href="#">To-DO-DO-DO</a> -->
-    </div>
-    <ul class="nav navbar-nav">
-    <!--  <li class="active"><a href="#">Home</a></li> -->
-      
-    </ul>
-  </div>
-</nav>
+
     </head>
      <nav class="navbar navbar-default">
     <div class="container-fluid">
