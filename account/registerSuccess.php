db14e90b75c4a286d11410d231eb2834a6e5031c
soumitad <sd655@njit.edu>
2017-05-08 10:20:15 -0400

    Made changes to reflect change in fontcolor (wht)

--- account/registerSuccess.php
+++ account/registerSuccess.php
@@ -3,8 +3,8 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-5 col-md-offset 6">
-                <?php echo 'You have successfully registered for this app. Please click on the '; ?>
-                <a href="Login.php">link</a><?php echo ' to login to the system by entering your username and password. '; ?>
+               <font color="white"> <?php echo 'You have successfully registered for this app. Please click on the '; ?> 
+                <a href="Login.php">link</a><?php echo ' to login to the system by entering your username and password. '; ?> </font>s
             </div>
         </div>    
     </div>
